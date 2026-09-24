865a8b2d48f4a9cfc6d852df34e646a467084618
Choicky ZHOU <zhoucaiqi@gmail.com>
2026-09-25 07:55:48 +0800

    Add MoQi lookup edge case tests

--- test/testpinyinhelper.cpp
+++ test/testpinyinhelper.cpp
@@ -51,9 +51,15 @@ int main() {
     auto moqiA =
         pinyinhelper->call<fcitx::IPinyinHelper::reverseLookupMoQi>("啊");
     FCITX_ASSERT(moqiA == "kk") << moqiA;
+    auto moqiExtB =
+        pinyinhelper->call<fcitx::IPinyinHelper::reverseLookupMoQi>("𠀀");
+    FCITX_ASSERT(moqiExtB == "av") << moqiExtB;
     auto moqiInvalid =
         pinyinhelper->call<fcitx::IPinyinHelper::reverseLookupMoQi>("AB");
     FCITX_ASSERT(moqiInvalid.empty()) << moqiInvalid;
+    auto moqiMissing =
+        pinyinhelper->call<fcitx::IPinyinHelper::reverseLookupMoQi>("😀");
+    FCITX_ASSERT(moqiMissing.empty()) << moqiMissing;
 
     return 0;
 }
